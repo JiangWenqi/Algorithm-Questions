521609c9694a8a2830df35c4fa69b158a95b23b1
姜文奇 <jiangwenqi1995@gmail.com>
2019-03-24 00:07:23 +0800

    update 107.binary-tree-level-order-traversal-ii.java

---
+++ 107.binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=107 lang=java
+ *
+ * [107] Binary Tree Level Order Traversal II
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Easy (45.85%)
+ * Total Accepted:    213.6K
+ * Total Submissions: 465.2K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the bottom-up level order traversal of its
+ * nodes' values. (ie, from left to right, level by level from leaf to root).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its bottom-up level order traversal as:
+ * 
+ * [
+ * ⁠ [15,7],
+ * ⁠ [9,20],
+ * ⁠ [3]
+ * ]
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+		if(root == null) return wrapList;
+		
+		queue.offer(root);
+		while(!queue.isEmpty()) {
+			int levelNum = queue.size();
+			List<Integer> subList = new LinkedList<Integer>();
+			for (int i = 0; i < levelNum; i++) {
+				if (queue.peek().left != null) queue.offer(queue.peek().left);
+				if (queue.peek().right != null) queue.offer(queue.peek().right);
+				subList.add(queue.poll().val);
+			}	
+			wrapList.add(0, subList);
+		}		
+		return wrapList;
+    }
+}
