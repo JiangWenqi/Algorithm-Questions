972dd5ef5c428df879ef83d370abd07ab2875b6c
姜文奇 <jiangwenqi1995@gmail.com>
2019-03-25 00:24:12 +0800

    update 111.minimum-depth-of-binary-tree.java

--- 111.minimum-depth-of-binary-tree.java
+++ 111.minimum-depth-of-binary-tree.java
@@ -43,10 +43,20 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-		if (root == null)	return 0;
-		int leftMinDepth = minDepth(root.left);
-		int rightMinDepth = minDepth(root.right);
-		return (leftMinDepth == 0 || rightMinDepth ==0) ?
-			leftMinDepth + rightMinDepth + 1 : Math.min(leftMinDepth, rightMinDepth) + 1;
-    }
+		if(root == null) return 0;
+
+        // leaf node
+        if(root.left == null && root.right == null) return 1;
+        
+        // leaf nodes are in right subtree
+        if(root.left == null) return minDepth(root.right) + 1;
+        
+        // leaf nodes are in left subtree
+        if(root.right == null) return minDepth(root.left) + 1;
+ 
+        // left/right subtrees both contains leaf nodes
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return Math.min(left, right) + 1;
+	}
 }
