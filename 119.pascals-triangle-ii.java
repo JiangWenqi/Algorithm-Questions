d1b9b54585f571adead9cb9f742125c012292110
姜文奇 <jiangwenqi1995@gmail.com>
2019-03-25 15:56:38 +0800

    update 119.pascals-triangle-ii.java

--- 119.pascals-triangle-ii.java
+++ 119.pascals-triangle-ii.java
@@ -34,13 +34,13 @@
  */
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-		List<Integer> row = new ArrayList<>();
-		for (int i = 0; i <= rowIndex; i++) {
-			row.add(1);
-			for (int j = i-1; j > 0; j--) {
-				row.set(j,row.get(j-1) + row.get(j));
-			}
+		ArrayList<Integer> row = new ArrayList<Integer>();
+        for (int i = 1; i <= rowIndex + 1; i++) {
+			row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+				row.set(j, row.get(j) + row.get(j+1));
+            } 
 		}
 		return row;
-    }
+	}
 }
