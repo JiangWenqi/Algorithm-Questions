3f2594be90247d4992ffa601010b3b55663a110d
姜文奇 <jiangwenqi1995@gmail.com>
2019-03-28 12:33:05 +0800

    update 155.min-stack.java

---
+++ 155.min-stack.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=155 lang=java
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (35.94%)
+ * Total Accepted:    279.1K
+ * Total Submissions: 773.3K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * 
+ * 
+ * pop() -- Removes the element on top of the stack.
+ * 
+ * 
+ * top() -- Get the top element.
+ * 
+ * 
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ */
+class MinStack {
+
+    /** initialize your data structure here. */
+	List<Integer> stack = null;
+
+    public MinStack() {
+        stack = new ArrayList<Integer>();
+    }
+    
+    public void push(int x) {
+        stack.add(x);
+    }
+    
+    public void pop() {
+        stack.remove(stack.size() - 1);
+    }
+    
+    public int top() {
+        return stack.get(stack.size() -1);
+    }
+    
+    public int getMin() {
+        int min = stack.get(0);
+		for (int i : stack) {
+			if (min > i)	min = i;
+		}
+		return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
