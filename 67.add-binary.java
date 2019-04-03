1aad0b7083cbf03d4307f574497a169eaa038647
姜文奇 <jiangwenqi1995@gmail.com>
2019-04-03 11:22:48 +0800

    update 67.add-binary.java

--- 67.add-binary.java
+++ 67.add-binary.java
@@ -1,45 +1,48 @@
 /*
- * @lc app=leetcode id=67 lang=java
+ * @lc app=leetcode.cn id=67 lang=java
  *
- * [67] Add Binary
+ * [67] 二进制求和
  *
- * https://leetcode.com/problems/add-binary/description/
+ * https://leetcode-cn.com/problems/add-binary/description/
  *
  * algorithms
- * Easy (38.19%)
- * Total Accepted:    282.1K
- * Total Submissions: 738.7K
+ * Easy (47.30%)
+ * Total Accepted:    20.3K
+ * Total Submissions: 43K
  * Testcase Example:  '"11"\n"1"'
  *
- * Given two binary strings, return their sum (also a binary string).
- * 
- * The input strings are both non-empty and contains only characters 1 or 0.
- * 
- * Example 1:
- * 
- * 
- * Input: a = "11", b = "1"
- * Output: "100"
- * 
- * Example 2:
- * 
- * 
- * Input: a = "1010", b = "1011"
- * Output: "10101"
- * 
+ * 给定两个二进制字符串，返回他们的和（用二进制表示）。
+ *
+ * 输入为非空字符串且只包含数字 1 和 0。
+ *
+ * 示例 1:
+ *
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ *
+ * 示例 2:
+ *
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ *
  */
 class Solution {
     public String addBinary(String a, String b) {
+        int aLength = a.length(), bLength = b.length();
         StringBuilder sb = new StringBuilder();
-	int i = a.length() - 1, j = b.length() - 1, carry = 0;
-	while ( i >= 0 || j >= 0) {
-	    int sum = carry;
-	    if ( i >= 0) sum += a.charAt(i--) - '0';
-	    if ( j >= 0) sum += b.charAt(j--) - '0';
-	    sb.append(sum % 2);
-	    carry = sum / 2;
-	}
-	if (carry != 0) sb.append(carry);
-	return sb.reverse().toString();
+        int carry = 0;
+        while (aLength > 0 || bLength > 0) {
+            int sum = carry;
+            if (aLength > 0) sum += a.charAt(--aLength) - '0';
+            if (bLength > 0) sum += b.charAt(--bLength) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        if (carry != 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
     }
 }
