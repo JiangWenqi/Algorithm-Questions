eb2a01274d1ad3496c345abb90ebaed1be9a0fb8
姜文奇 <jiangwenqi1995@gmail.com>
2019-04-04 13:26:29 +0800

    update 69.sqrtx.java

--- 69.sqrtx.java
+++ 69.sqrtx.java
@@ -1,47 +1,43 @@
 /*
- * @lc app=leetcode id=69 lang=java
+ * @lc app=leetcode.cn id=69 lang=java
  *
- * [69] Sqrt(x)
+ * [69] x 的平方根
  *
- * https://leetcode.com/problems/sqrtx/description/
+ * https://leetcode-cn.com/problems/sqrtx/description/
  *
  * algorithms
- * Easy (30.83%)
- * Total Accepted:    339.4K
- * Total Submissions: 1.1M
+ * Easy (35.23%)
+ * Total Accepted:    29.5K
+ * Total Submissions: 83.7K
  * Testcase Example:  '4'
  *
- * Implement int sqrt(int x).
+ * 实现 int sqrt(int x) 函数。
  * 
- * Compute and return the square root of x, where x is guaranteed to be a
- * non-negative integer.
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
  * 
- * Since the return type is an integer, the decimal digits are truncated and
- * only the integer part of the result is returned.
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
  * 
- * Example 1:
+ * 示例 1:
  * 
- * 
- * Input: 4
- * Output: 2
+ * 输入: 4
+ * 输出: 2
  * 
  * 
- * Example 2:
+ * 示例 2:
  * 
- * 
- * Input: 8
- * Output: 2
- * Explanation: The square root of 8 is 2.82842..., and since 
- * the decimal part is truncated, 2 is returned.
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842..., 
+ * 由于返回类型是整数，小数部分将被舍去。
  * 
  * 
  */
 class Solution {
     public int mySqrt(int x) {
-        long r = x;
-	while(r * r > x) {
-	    r = (r + x/r)  / 2;
-	}  
-	return (int)r;
+		long result = x;
+		while (result * result > x) {
+		   result = (result + x / result) / 2;
+		}
+		return (int)result;
     }
 }
