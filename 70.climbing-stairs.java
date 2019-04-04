1a781feedaa9606eff17c6b801b2ee4c95c82317
姜文奇 <jiangwenqi1995@gmail.com>
2019-04-04 13:51:35 +0800

    update 70.climbing-stairs.java

--- 70.climbing-stairs.java
+++ 70.climbing-stairs.java
@@ -1,56 +1,50 @@
 /*
- * @lc app=leetcode id=70 lang=java
+ * @lc app=leetcode.cn id=70 lang=java
  *
- * [70] Climbing Stairs
+ * [70] 爬楼梯
  *
- * https://leetcode.com/problems/climbing-stairs/description/
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
  *
  * algorithms
- * Easy (43.64%)
- * Total Accepted:    368.7K
- * Total Submissions: 844.6K
+ * Easy (44.89%)
+ * Total Accepted:    39.5K
+ * Total Submissions: 87.9K
  * Testcase Example:  '2'
  *
- * You are climbing a stair case. It takes n steps to reach to the top.
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * 
- * Each time you can either climb 1 or 2 steps. In how many distinct ways can
- * you climb to the top?
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  * 
- * Note: Given n will be a positive integer.
+ * 注意：给定 n 是一个正整数。
  * 
- * Example 1:
+ * 示例 1：
  * 
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
  * 
- * Input: 2
- * Output: 2
- * Explanation: There are two ways to climb to the top.
- * 1. 1 step + 1 step
- * 2. 2 steps
+ * 示例 2：
  * 
- * 
- * Example 2:
- * 
- * 
- * Input: 3
- * Output: 3
- * Explanation: There are three ways to climb to the top.
- * 1. 1 step + 1 step + 1 step
- * 2. 1 step + 2 steps
- * 3. 2 steps + 1 step
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
  * 
  * 
  */
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1 || n == 2 || n == 3) return n;
-	int[] mem = new int[n];
-	mem[0] = 1;
-	mem[1] = 2;
-	mem[2] = 3;
-	for ( int i = 3; i < n;i++) {
-	    mem[i] = mem[i-1] + mem[i-2];
-	} 
-	
-	return mem[n-1];
-    }
+		if (n == 1)	return 1;
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		return dp[n];
+	}
 }
