83453e15e4e83bb25dd6184cb599e7ef6bb178e3
姜文奇 <jiangwenqi1995@gmail.com>
2019-04-08 12:28:44 +0800

    update 88.merge-sorted-array.java

--- 88.merge-sorted-array.java
+++ 88.merge-sorted-array.java
@@ -1,44 +1,46 @@
 /*
- * @lc app=leetcode id=88 lang=java
+ * @lc app=leetcode.cn id=88 lang=java
  *
- * [88] Merge Sorted Array
+ * [88] 合并两个有序数组
  *
- * https://leetcode.com/problems/merge-sorted-array/description/
+ * https://leetcode-cn.com/problems/merge-sorted-array/description/
  *
  * algorithms
- * Easy (35.03%)
- * Total Accepted:    339.2K
- * Total Submissions: 967K
+ * Easy (43.45%)
+ * Total Accepted:    37.7K
+ * Total Submissions: 86.5K
  * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
  *
- * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as
- * one sorted array.
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
  * 
- * Note:
- * 
- * 
- * The number of elements initialized in nums1 and nums2 are m and n
- * respectively.
- * You may assume that nums1 has enough space (size that is greater or equal to
- * m + n) to hold additional elements from nums2.
+ * 说明:
  * 
  * 
- * Example:
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
  * 
  * 
- * Input:
+ * 示例:
+ * 
+ * 输入:
  * nums1 = [1,2,3,0,0,0], m = 3
  * nums2 = [2,5,6],       n = 3
  * 
- * Output: [1,2,2,3,5,6]
- * 
+ * 输出: [1,2,2,3,5,6]
  * 
  */
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-	while (n > 0) {
-	    // 从后往前排，最后一个数是最大的
-	    nums1[m+n-1] = (m == 0 || nums2[n-1] > nums1[m-1]) ? nums2[--n] : nums1[--m];
-	}
+		while (m > 0 && n > 0) {
+			nums1[m+n-1] = (nums2[n-1] > nums1[m-1]) ? nums2[--n] : nums1[--m];
+		}
+
+		while (m > 0) {
+			nums1[m+n-1] = nums1[--m];
+		}
+
+		while (n > 0) {
+			nums1[m+n-1] = nums2[--n];
+		}
     }
 }
