3bfe69121682609825575bfb43428902225c071f
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-06-13 11:05:41 +0200

    20220613

--- 120.triangle.cpp
+++ 120.triangle.cpp
@@ -66,11 +66,13 @@ using namespace std;
 class Solution {
 public:
   int minimumTotal(vector<vector<int>> &triangle) {
-    vector<int> mini = triangle[triangle.size() - 1];
-    for (int i = triangle.size() - 2; i >= 0; --i)
-      for (int j = 0; j < triangle[i].size(); ++j)
-        mini[j] = triangle[i][j] + min(mini[j], mini[j + 1]);
-    return mini[0];
+    int rows = triangle.size();
+    for (int i = rows - 2; i >= 0; i--) {
+      for (int j = 0; j <= i; j++) {
+        triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+      }
+    }
+    return triangle[0][0];
   }
 };
 // @lc code=end
