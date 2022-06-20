66dc4a78a1fd97ab15301bf9e0ed6255decbf47a
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-06-20 20:11:24 +0200

    20220620

--- 572.subtree-of-another-tree.java
+++ 572.subtree-of-another-tree.java
@@ -1,5 +1,5 @@
 /*
- * @lc app=leetcode id=572 lang=cpp
+ * @lc app=leetcode id=572 lang=java
  *
  * [572] Subtree of Another Tree
  *
@@ -9,73 +9,75 @@
  * Easy (45.51%)
  * Likes:    5508
  * Dislikes: 298
- * Total Accepted:    490.7K
+ * Total Accepted:    490.8K
  * Total Submissions: 1.1M
  * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
  *
  * Given the roots of two binary trees root and subRoot, return true if there
  * is a subtree of root with the same structure and node values of subRoot and
  * false otherwise.
- *
+ * 
  * A subtree of a binary tree tree is a tree that consists of a node in tree
  * and all of this node's descendants. The tree tree could also be considered
  * as a subtree of itself.
- *
- *
+ * 
+ * 
  * Example 1:
- *
- *
+ * 
+ * 
  * Input: root = [3,4,5,1,2], subRoot = [4,1,2]
  * Output: true
- *
- *
+ * 
+ * 
  * Example 2:
- *
- *
+ * 
+ * 
  * Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
  * Output: false
- *
- *
- *
+ * 
+ * 
+ * 
  * Constraints:
- *
- *
+ * 
+ * 
  * The number of nodes in the root tree is in the range [1, 2000].
  * The number of nodes in the subRoot tree is in the range [1, 1000].
  * -10^4 <= root.val <= 10^4
  * -10^4 <= subRoot.val <= 10^4
- *
- *
+ * 
+ * 
  */
 
 // @lc code=start
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
  */
 class Solution {
- private:
-  bool check(TreeNode* a, TreeNode* b) {
-    if (a == NULL && b == NULL)
-      return true;
-    else if (a == NULL || b == NULL || a->val != b->val)
-      return false;
-    else
-      return check(a->left, b->left) && check(a->right, b->right);
-  }
+    private boolean check(TreeNode a, TreeNode b) {
+        if (a == null && b == null)
+            return true;
+        else if (a == null || b == null || a.val != b.val)
+            return false;
+        else
+            return check(a.left, b.left) && check(a.right, b.right);
+    }
 
- public:
-  bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-    if (root == NULL) return subRoot == NULL;
-    return check(root, subRoot) || isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-  }
-};
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null)
+            return subRoot == null;
+        return check(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+}
 // @lc code=end
