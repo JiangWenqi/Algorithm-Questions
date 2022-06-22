5c9e0c31c33ed943a4532f39b1fbbe31a31f2584
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-06-22 21:06:40 +0200

    20220622

--- 28.implement-str-str.java
+++ 28.implement-str-str.java
@@ -7,16 +7,33 @@
 // @lc code=start
 class Solution {
     public int strStr(String haystack, String needle) {
-        for (int i = 0;; i++) {
-            for (int j = 0;; j++) {
-                if (j == needle.length())
-                    return i;
-                if (i + j == haystack.length())
-                    return -1;
-                if (needle.charAt(j) != haystack.charAt(i + j))
-                    break;
+        if (needle.length() < 1)
+            return 0;
+        int m = needle.length(), n = haystack.length();
+        char[] p = (" " + needle).toCharArray();
+        char[] s = (" " + haystack).toCharArray();
+        int[] next = new int[m + 10];
+        // 预处理p的next数组
+        // next[i]记录p[]数组中以i结尾的最大后缀等于最大前缀时的最大前缀的末尾位置
+        for (int i = 2, j = 0; i <= m; i++) {
+            while (j != 0 && p[i] != p[j + 1])
+                j = next[j];
+            if (p[i] == p[j + 1])
+                j++;
+            next[i] = j;
+        }
+        // 匹配
+        for (int i = 1, j = 0; i <= n; i++) {
+            while (j != 0 && s[i] != p[j + 1])
+                j = next[j];
+            if (s[i] == p[j + 1])
+                j++;
+            if (j == m) {
+                j = next[j];
+                return i - m;
             }
         }
+        return -1;
     }
 }
 // @lc code=end
