97ff577ecd1df565b29143603e8c97a901ba9d6d
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-08-28 14:02:36 +0200

    20220828

---
+++ Leetcode-Solutions/155.min-stack.cpp
@@ -0,0 +1,134 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem155.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode id=155 lang=cpp
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Medium (51.29%)
+ * Likes:    9331
+ * Dislikes: 664
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ *
+ * Implement the MinStack class:
+ *
+ *
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
+ *
+ *
+ * You must implement a solution with O(1) time complexity for each
+ * function.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ *
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ *
+ * Explanation
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin(); // return -3
+ * minStack.pop();
+ * minStack.top();    // return 0
+ * minStack.getMin(); // return -2
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= val <= 2^31 - 1
+ * Methods pop, top and getMin operations will always be called on non-empty
+ * stacks.
+ * At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
+ *
+ *
+ */
+
+
+ /*
+class MinStack {
+private:
+    stack<int> valStk, minStk;
+
+public:
+    MinStack() {
+
+    }
+
+    void push(int val) {
+        valStk.push(val);
+        if (minStk.empty() || val <= minStk.top()) minStk.push(val);
+    }
+
+    void pop() {
+        if (valStk.top() == minStk.top()) minStk.pop();
+        valStk.pop();
+    }
+
+    int top() {
+        return valStk.top();
+    }
+
+    int getMin() {
+        return minStk.top();
+    }
+};
+*/
+// @lc code=start
+class MinStack {
+public:
+    vector< pair<int, int> > s;
+
+    MinStack() { }
+
+    void push(int val) {
+        if (s.empty())
+            s.push_back({ val, val });
+        else
+            s.push_back({ val, min(s.back().second, val) });
+    }
+
+    void pop() { s.pop_back(); }
+
+    int top() { return s.back().first; }
+
+    int getMin() { return s.back().second; }
+};
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+ // @lc code=end
+
---
+++ Leetcode-Solutions/739.daily-temperatures.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode id=739 lang=java
+ *
+ * [739] Daily Temperatures
+ *
+ * https://leetcode.com/problems/daily-temperatures/description/
+ *
+ * algorithms
+ * Medium (67.72%)
+ * Likes:    7951
+ * Dislikes: 172
+ * Total Accepted:    451.6K
+ * Total Submissions: 666.8K
+ * Testcase Example:  '[73,74,75,71,69,72,76,73]'
+ *
+ * Given an array of integers temperatures represents the daily temperatures,
+ * return an array answer such that answer[i] is the number of days you have to
+ * wait after the i^th day to get a warmer temperature. If there is no future
+ * day for which this is possible, keep answer[i] == 0 instead.
+ * 
+ * 
+ * Example 1:
+ * Input: temperatures = [73,74,75,71,69,72,76,73]
+ * Output: [1,1,4,2,1,1,0,0]
+ * Example 2:
+ * Input: temperatures = [30,40,50,60]
+ * Output: [1,1,1,0]
+ * Example 3:
+ * Input: temperatures = [30,60,90]
+ * Output: [1,1,0]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= temperatures.length <= 10^5
+ * 30 <= temperatures[i] <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> stk = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stk.empty() && temperatures[stk.peek()] <= temperatures[i])
+                stk.pop();
+            if (!stk.empty())
+                res[i] = stk.peek() - i;
+            stk.push(i);
+        }
+        return res;
+    }
+}
+// @lc code=end
