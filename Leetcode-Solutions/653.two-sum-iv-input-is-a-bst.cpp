bc8bd3c319b6674627ef6f4e9d7cebf3f2a995f3
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-10-09 15:57:08 +0200

    20221009

---
+++ Leetcode-Solutions/653.two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=653 lang=cpp
+ *
+ * [653] Two Sum IV - Input is a BST
+ *
+ * https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+ *
+ * algorithms
+ * Easy (59.43%)
+ * Likes:    5004
+ * Dislikes: 223
+ * Total Accepted:    392.4K
+ * Total Submissions: 649.3K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n9'
+ *
+ * Given the root of a Binary Search Tree and a target number k, return true if
+ * there exist two elements in the BST such that their sum is equal to the
+ * given target.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [5,3,6,2,4,null,7], k = 9
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [5,3,6,2,4,null,7], k = 28
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 10^4].
+ * -10^4 <= Node.val <= 10^4
+ * root is guaranteed to be a valid binary search tree.
+ * -10^5 <= k <= 10^5
+ *
+ *
+ */
+
+ // @lc code=start
+ /**
+  * Definition for a binary tree node.
+  * struct TreeNode {
+  *     int val;
+  *     TreeNode *left;
+  *     TreeNode *right;
+  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  * };
+  */
+class Solution {
+private:
+    unordered_set<int> st;
+
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+        if (st.count(k - root->val)) return true;
+        st.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};
+// @lc code=end
+
