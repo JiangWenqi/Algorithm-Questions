7121e7a258d883e0422d7640dc02fa263691ea93
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-10-21 15:57:17 +0200

    20221021

---
+++ Leetcode-Solutions/219.contains-duplicate-ii.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=219 lang=cpp
+ *
+ * [219] Contains Duplicate II
+ *
+ * https://leetcode.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (40.92%)
+ * Likes:    3660
+ * Dislikes: 2262
+ * Total Accepted:    563.8K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * Given an integer array nums and an integer k, return true if there are two
+ * distinct indices i and j in the array such that nums[i] == nums[j] and abs(i
+ * - j) <= k.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * 0 <= k <= 10^5
+ *
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            if (mp.count(num) && i - mp[num] <= k)
+            {
+                    return true;
+            }
+            else
+            {
+                mp[num] = i;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
