951e796a76b59cf0aaff37dbd1a67bdc0a7d66dc
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-11-18 13:47:30 +0100

    20221118

---
+++ Leetcode-Solutions/263.ugly-number.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=263 lang=cpp
+ *
+ * [263] Ugly Number
+ *
+ * https://leetcode.com/problems/ugly-number/description/
+ *
+ * algorithms
+ * Easy (41.62%)
+ * Likes:    2248
+ * Dislikes: 1345
+ * Total Accepted:    367.9K
+ * Total Submissions: 871.3K
+ * Testcase Example:  '6'
+ *
+ * An ugly number is a positive integer whose prime factors are limited to 2,
+ * 3, and 5.
+ *
+ * Given an integer n, return true if n is an ugly number.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 6
+ * Output: true
+ * Explanation: 6 = 2 × 3
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 1
+ * Output: true
+ * Explanation: 1 has no prime factors, therefore all of its prime factors are
+ * limited to 2, 3, and 5.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: n = 14
+ * Output: false
+ * Explanation: 14 is not ugly since it includes the prime factor 7.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= n <= 2^31 - 1
+ *
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (num <= 0)
+            return false;
+        while (num % 2 == 0)
+            num /= 2;
+        while (num % 3 == 0)
+            num /= 3;
+        while (num % 5 == 0)
+            num /= 5;
+        return num == 1;
+    }
+};
+// @lc code=end
