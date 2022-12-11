57d0c86c86d118c7fd20e1c7d3846f94706c7cb8
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-12-11 11:50:08 +0100

    20221211

---
+++ Leetcode-Solutions/124.binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=124 lang=cpp
+ *
+ * [124] Binary Tree Maximum Path Sum
+ *
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (38.50%)
+ * Likes:    12696
+ * Dislikes: 608
+ * Total Accepted:    893.1K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * A path in a binary tree is a sequence of nodes where each pair of adjacent
+ * nodes in the sequence has an edge connecting them. A node can only appear in
+ * the sequence at most once. Note that the path does not need to pass through
+ * the root.
+ *
+ * The path sum of a path is the sum of the node's values in the path.
+ *
+ * Given the root of a binary tree, return the maximum path sum of any
+ * non-empty path.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,2,3]
+ * Output: 6
+ * Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 =
+ * 6.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [-10,9,20,null,null,15,7]
+ * Output: 42
+ * Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 +
+ * 7 = 42.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 3 * 10^4].
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+    int res = INT_MIN;
+    int dfs(TreeNode *node)
+    {
+        if (!node)
+            return 0;
+        int l = max(dfs(node->left), 0), r = max(dfs(node->right), 0);
+        res = max(res, l + r + node->val);
+        return node->val + max(l, r);
+    }
+
+public:
+    int maxPathSum(TreeNode *root)
+    {
+        dfs(root);
+        return res;
+    }
+};
+// @lc code=end
